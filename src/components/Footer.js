1ef9d819d00e5f204538a3966b083a2070fd026b
Zacharyjg <guldb016@umn.edu>
2021-08-12 01:05:30 -0500

    Added basic footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Proudly built using React.js
+      </p>
+    </footer>
+  );
+}
