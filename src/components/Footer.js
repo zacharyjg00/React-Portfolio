e1aacd39c370aa188623eded5d41cd1ff6de4901
Zacharyjg <guldb016@umn.edu>
2021-08-13 18:20:22 -0500

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer>
-      <h6 className="footer-text">Copyright © Zachary Guldberg 2021</h6>
-    </footer>
-  );
-}
--- src/pages/Resume.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Resume() {
-    return (
-        <div className="main-component">
-            <h3>A link to download my resume can be found below</h3>
-        </div>
-    )
-}
