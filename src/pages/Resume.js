fd2d6a086123208176b34708606c8a22bdb03ed7
Zacharyjg <guldb016@umn.edu>
2021-08-13 17:33:25 -0500

    Added text for resume download link preemptively

---
+++ src/pages/Resume.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <div className="main-component">
+            <h3>A link to download my resume can be found below</h3>
+        </div>
+    )
+}
