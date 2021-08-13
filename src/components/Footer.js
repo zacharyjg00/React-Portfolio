f0be67f8d59255756526a7db689bccb698363cd1
Zacharyjg <guldb016@umn.edu>
2021-08-13 17:43:29 -0500

    Changed small styling bit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      <h6>Copyright © Zachary Guldberg 2021</h6>
+      <h6 className="footer-text">Copyright © Zachary Guldberg 2021</h6>
     </footer>
   );
 }
