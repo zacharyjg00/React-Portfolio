17dc47cc817fb4c2b6a5fdce669c88160aae042a
Zacharyjg <guldb016@umn.edu>
2021-08-16 14:57:35 -0500

    Deploying

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,45 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 export default function Navigation() {
     return (
         <ul className="nav nav-tabs">
             <li className="nav-item">
-                <a
-                    href={"/"}
+                <Link
+                    to="/"
                     // This is a conditional (ternary) operator that checks to see if the current page is "About me"
                     // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-                    className={window.location.pathname ==='/' ? 'nav-link active' : 'nav-link'}
+                    className={window.location.pathname === '/' ? 'nav-link active' : 'nav-link'}
                 >
                     About me
-                </a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a
-                    href={"/portfolio"}
+                <Link
+                    to="/portfolio"
                     // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={window.location.pathname === '/portfolio' ? 'nav-link active' : 'nav-link'}
                 >
                     Portfolio
-                </a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a
-                    href={"/contact"}
+                <Link
+                    to="/contact"
                     // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={window.location.pathname === '/contact' ? 'nav-link active' : 'nav-link'}
                 >
                     Contact
-                </a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a
-                    href={"/resume"}
+                <Link
+                    to="/resume"
                     // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={window.location.pathname === '/resume' ? 'nav-link active' : 'nav-link'}
                 >
                     Resume
-                </a>
+                </Link>
             </li>
         </ul>
     );
